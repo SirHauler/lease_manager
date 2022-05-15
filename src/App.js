d87b2c3264f3d02a199de3f34a9b69b94facfedc
Alexander Alvarado-Barahona <realalex@DN0a1fa362.SUNet>
2022-05-15 15:05:06 -0700

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          So this is the new version?
         </p>
         <a
           className="App-link"
